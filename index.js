8334c856860ce1e4c3d33a47b2f9cc3d659630af
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2023-08-10 19:20:27 -0600

    Rutas del Servicio

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-const doctores = require('./routes/doctores');
-app.use('/api/doctores', doctores);
-
+// Keven
 const roles = require('./routes/roles');
 app.use('/api/roles', roles);
 
@@ -14,5 +12,43 @@ app.use('/api/usuarios', usuarios);
 const tokens = require('./routes/tokens');
 app.use('/api/tokens', tokens);
 
+//Jorge
+const tipoMensaje = require('./routes/tipoMensaje');
+app.use('/api/tipoMensaje', tipoMensaje);
+
+const mensaje = require('./routes/mensaje');
+app.use('/api/mensaje', mensaje);
+
+//Griselda
+
+const pacientes = require('./routes/pacientes');
+app.use('/api/pacientes', pacientes);
+
+const citas = require('./routes/citas');
+app.use('/api/citas', citas);
+
+//Cristian
+
+const doctores = require('./routes/doctores');
+app.use('/api/doctores', doctores);
+
+const especialidades = require('./routes/especialidades');
+app.use('/api/especialidades', especialidades);
+
+// Marcos 
+
+const estadoCita = require('./routes/estadoCita');
+app.use('/api/estadoCita', estadoCita);
+
+const doctoresEspecialidades = require('./routes/doctoresEspecialidades');
+app.use('/api/doctoresEspecialidades', doctoresEspecialidades);
+
+//David 
+
+const rutas = require('./routes/rutas');
+app.use('/api/rutas', rutas);
+
+const permisos = require('./routes/permisos');
+app.use('/api/permisos', permisos);
 
 app.listen(3000);
