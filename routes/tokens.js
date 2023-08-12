bc452dde12856bfb7c6e397afbe6bb07ed8f3152
keven-bardales <122845766+keven-bardales@users.noreply.github.com>
2023-08-11 22:45:48 -0600

    usuuarios y tokens

---
+++ routes/tokens.js
@@ -0,0 +1,68 @@
+
+// importamos express
+const express = require('express');
+// creamos un nuevo router de express para exportarlo al final del archivo
+const app = express.Router();
+// importamos la conexion de la base de datos
+const db = require('../database/conn');
+// importamos el error para mostrar errores de consola
+const { error } = require('console');
+
+app.post('/', async (req, res) => {
+    const { usuario_id, token, fecha_inicio, fecha_expiracion } = req.body;
+  
+    if (!usuario_id || !token || !fecha_inicio || !fecha_expiracion) {
+      return res.status(400).json({ message: 'usuario_id, token, fecha_inicio, y fecha_expiracion son requeridos' });
+    }
+  
+    try {
+      await db.none('INSERT INTO tbl_tokens (usuario_id, token, fecha_inicio, fecha_expiracion) VALUES ($1, $2, $3, $4)', [usuario_id, token, fecha_inicio, fecha_expiracion]);
+      res.status(201).json({ message: 'Token creado exitosamente' });
+    } catch (err) {
+      console.error({ ERROR: err, RUTA: '/tokens', METODO: 'POST' });
+      res.status(500).json({ message: 'Error al crear el token' });
+    }
+  });
+
+
+  app.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { usuario_id, token, fecha_inicio, fecha_expiracion } = req.body;
+  
+    try {
+      const updated = await db.result('UPDATE tbl_tokens SET usuario_id = $1, token = $2, fecha_inicio = $3, fecha_expiracion = $4 WHERE id = $5', [usuario_id, token, fecha_inicio, fecha_expiracion, id]);
+  
+      if (updated.rowCount === 0) {
+        return res.status(404).json({ message: 'Token no encontrado' });
+      }
+  
+      res.status(200).json({ message: 'Token actualizado exitosamente' });
+    } catch (err) {
+      console.error({ ERROR: err, RUTA: `/tokens/${id}`, METODO: 'PUT' });
+      res.status(500).json({ message: 'Error al actualizar el token' });
+    }
+  });
+
+
+  app.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      const deleted = await db.result('DELETE FROM tbl_tokens WHERE id = $1', [id]);
+  
+      if (deleted.rowCount === 0) {
+        return res.status(404).json({ message: 'Token no encontrado' });
+      }
+  
+      res.status(200).json({ message: 'Token eliminado exitosamente' });
+    } catch (err) {
+      console.error({ ERROR: err, RUTA: `/tokens/${id}`, METODO: 'DELETE' });
+      res.status(500).json({ message: 'Error al eliminar el token' });
+    }
+  });
+  
+  
+
+  
+module.exports = app;
+  
---
+++ routes/usuarios.js
@@ -0,0 +1,105 @@
+
+// importamos express
+const express = require('express');
+// creamos un nuevo router de express para exportarlo al final del archivo
+const app = express.Router();
+// importamos la conexion de la base de datos
+const db = require('../database/conn');
+// importamos el error para mostrar errores de consola
+const { error } = require('console');
+
+
+app.post('/', async (req, res) => {
+    const { nombre, email, telefono, password, secondPassword } = req.body;
+  
+    if (!nombre || !email || !password) {
+      return res.status(400).json({ message: 'Nombre, email y password son requeridos' });
+    }
+
+    if(password !== secondPassword) {
+        return res.status(400).json({ message: 'Contraseña incorrecta' });
+    }
+  
+    try {
+
+        // TODO tener una biblioteca para hacer hash de la password
+     //  const hashedPassword = await bcrypt.hash(password, 10); Asegúrate de usar una biblioteca de hashing segura
+      await db.none('INSERT INTO tbl_usuarios (nombre, email, telefono, password) VALUES ($1, $2, $3, $4)', [nombre, email, telefono, password]);
+      res.status(201).json({ message: 'Usuario creado exitosamente' });
+    } catch (err) {
+      console.error({ ERROR: err, RUTA: '/usuarios', METODO: 'POST' });
+      res.status(500).json({ message: 'Error al crear el usuario' });
+    }
+  });
+
+  app.get('/', async (req, res) => {
+    try {
+      const usuarios = await db.manyOrNone('SELECT * FROM tbl_usuarios WHERE estado = true');
+      res.status(200).json(usuarios);
+    } catch (err) {
+      console.error({ ERROR: err, RUTA: '/usuarios', METODO: 'GET' });
+      res.status(500).json({ message: 'Error al obtener los usuarios' });
+    }
+  });
+  
+
+  app.get('/:id', async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      const usuario = await db.oneOrNone('SELECT * FROM tbl_usuarios WHERE id = $1 AND estado = true', [id]);
+  
+      if (!usuario) {
+        return res.status(404).json({ message: 'Usuario no encontrado' });
+      }
+  
+      res.status(200).json(usuario);
+    } catch (err) {
+      console.error({ ERROR: err, RUTA: `/usuarios/${id}`, METODO: 'GET' });
+      res.status(500).json({ message: 'Error al obtener el usuario' });
+    }
+  });
+
+  app.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { nombre, email, telefono, password } = req.body;
+  
+    try {
+        // TODO implementar el hash de la password
+    //   const hashedPassword = password ? await bcrypt.hash(password, 10) : null; // Si se actualiza la contraseña, se hace hash de ella
+
+      const updated = await db.result('UPDATE tbl_usuarios SET nombre = $1, email = $2, telefono = $3, password = $4 WHERE id = $5 AND estado = true', [nombre, email, telefono, password, id]);
+  
+      if (updated.rowCount === 0) {
+        return res.status(404).json({ message: 'Usuario no encontrado o no está activo' });
+      }
+  
+      res.status(200).json({ message: 'Usuario actualizado exitosamente' });
+    } catch (err) {
+      console.error({ ERROR: err, RUTA: `/usuarios/${id}`, METODO: 'PUT' });
+      res.status(500).json({ message: 'Error al actualizar el usuario' });
+    }
+  });
+  
+  app.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      const updated = await db.result('UPDATE tbl_usuarios SET estado = false, fecha_borra = CURRENT_TIMESTAMP WHERE id = $1', [id]);
+  
+      if (updated.rowCount === 0) {
+        return res.status(404).json({ message: 'Usuario no encontrado' });
+      }
+  
+      res.status(200).json({ message: 'Usuario marcado como inactivo exitosamente' });
+    } catch (err) {
+      console.error({ ERROR: err, RUTA: `/usuarios/${id}`, METODO: 'DELETE' });
+      res.status(500).json({ message: 'Error al marcar el usuario como inactivo' });
+    }
+  });
+  
+  
+  
+
+
+module.exports = app;
