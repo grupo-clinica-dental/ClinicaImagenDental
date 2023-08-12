03c2d66b38026ccae25f7a0d7b90490216b60d81
CristianMartinez1398 <109698231+CristianMartinez1398@users.noreply.github.com>
2023-08-11 19:21:28 -0600

    Cristian
    Terminadas todas las tablas programadas

--- routes/especialidades.js
+++ routes/especialidades.js
@@ -72,6 +72,117 @@ app.post('', (req, res) => {
 
 });
 
+app.put('/:id', (req, res) => {
+
+
+    const parametros = [
+
+        req.body.nombre,
+        req.body.fecha_borrado,
+        req.params.id
+
+    ];
+
+    let sql = `  
+                    update tbl_especialidades 
+                    set nombre = $1, fecha_borrado = $2 
+                    where id = $3
+
+                `;
+
+    let mensajes = new Array();
+
+    let respuestaValidacion = {
+
+        exito: true,
+        mensaje: mensajes,
+        excepcion: "",
+        item_rol: ""
+
+    };
+
+    db.result(sql, parametros, r => r.rowCount)
+        .then(data => {
+
+            const objetoMod = {
+
+                id: req.params.id,
+                nombre: req.body.nombre,
+                fecha_borrado:req.body.fecha_borrado,
+
+            }
+
+            respuestaValidacion.mensaje = "Operación Exitosa";
+            respuestaValidacion.item_rol = objetoMod;
+            res.json(respuestaValidacion);
+
+        })
+        .catch((error) => {
+
+            respuestaValidacion.exito = false;
+            respuestaValidacion.mensaje.push("Operación Erronea");
+            respuestaValidacion.excepcion = error.message;
+            res.status(500).json(error);
+
+        }
+        );
+
+});
+
+app.delete('/:id', (req, res) => {
+
+
+    const parametros = [
+
+        req.params.id
+
+    ];
+
+    let sql = `  
+                    update tbl_especialidades 
+                    set activo = false
+                    where id = $1
+
+                `;
+
+    let mensajes = new Array();
+
+    let respuestaValidacion = {
+
+        exito: true,
+        mensaje: mensajes,
+        excepcion: "",
+        item_rol: ""
+
+    };
+
+    db.result(sql, parametros, r => r.rowCount)
+        .then(data => {
+
+            const objetoMod = {
+
+                id: req.params.id,
+                nombre: req.body.nombre,
+                fecha_borrado:req.body.fecha_borrado,
+                activo: false
+            }
+
+            respuestaValidacion.mensaje = "Operación Exitosa";
+            respuestaValidacion.item_rol = objetoMod;
+            res.json(respuestaValidacion);
+
+        })
+        .catch((error) => {
+
+            respuestaValidacion.exito = false;
+            respuestaValidacion.mensaje.push("Operación Erronea");
+            respuestaValidacion.excepcion = error.message;
+            res.status(500).json(error);
+
+        }
+        );
+
+});
 
 
 
