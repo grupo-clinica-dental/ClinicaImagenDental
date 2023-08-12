fc16c36212ee02ea25043c7b6fe87a1b168b9d73
godoymarcos27 <duabua1@gmail.com>
2023-08-11 23:54:19 -0600

    crud_estado_citas

--- routes/estadoCita.js
+++ routes/estadoCita.js
@@ -3,18 +3,74 @@ const app = express.Router();
 const db = require('../database/conn');
 
 
-app.get('/', async (req, res) => {
-    try {
-      // Consultamos todos los roles activos en la base de datos
-      const estadoCita = await db.manyOrNone('SELECT * FROM tbl_estados_cita where activo = true');
-      // Enviamos un estado 200 con los roles obtenidos
-      res.status(200).json(estadoCita);
-    } catch (err) {
-      // Capturamos el error y mostramos la información en la consola
-      console.error({ERROR: err, RUTA: '/estadoCita', METODO: 'GET'});
-      // Enviamos un estado 500 indicando un error en el servidor
-      res.status(500).json({message: 'Error al obtener los estado_cita'});
-    }
-  });
+
+// Get all active appointment states
+app.get('/estadosCita', async (req, res) => {
+  try {
+    const estadoCita = await db.manyOrNone('SELECT * FROM tbl_estados_cita WHERE activo = true');
+    res.status(200).json(estadoCita);
+  } catch (err) {
+    console.error({ ERROR: err, RUTA: '/estadosCita', METODO: 'GET' });
+    res.status(500).json({ message: 'Error al obtener los estado_cita' });
+  }
+});
+
+// Create a new appointment state
+app.post('/estadosCita', async (req, res) => {
+  const { estado } = req.body;
+  if (!estado) {
+    return res.status(400).json({ message: 'El estado es requerido' });
+  }
+
+  try {
+    const newEstadoCita = await db.one(
+      'INSERT INTO tbl_estados_cita (estado) VALUES ($1) RETURNING *',
+      estado
+    );
+    res.status(201).json(newEstadoCita);
+  } catch (err) {
+    console.error({ ERROR: err, RUTA: '/estadosCita', METODO: 'POST' });
+    res.status(500).json({ message: 'Error al crear el estado_cita' });
+  }
+});
+
+// Update an appointment state
+app.put('/estadosCita/:id', async (req, res) => {
+  const { id } = req.params;
+  const { estado } = req.body;
+
+  if (!estado) {
+    return res.status(400).json({ message: 'El estado es requerido' });
+  }
+
+  try {
+    const updatedEstadoCita = await db.one(
+      'UPDATE tbl_estados_cita SET estado = $1 WHERE id = $2 RETURNING *',
+      [estado, id]
+    );
+    res.status(200).json(updatedEstadoCita);
+  } catch (err) {
+    console.error({ ERROR: err, RUTA: `/estadosCita/${id}`, METODO: 'PUT' });
+    res.status(500).json({ message: 'Error al actualizar el estado_cita' });
+  }
+});
+
+// Delete an appointment state (set activo to false)
+app.delete('/estadosCita/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedEstadoCita = await db.one(
+      'UPDATE tbl_estados_cita SET activo = false WHERE id = $1 RETURNING *',
+      id
+    );
+    res.status(200).json(deletedEstadoCita);
+  } catch (err) {
+    console.error({ ERROR: err, RUTA: `/estadosCita/${id}`, METODO: 'DELETE' });
+    res.status(500).json({ message: 'Error al eliminar el estado_cita' });
+  }
+});
+
+// Start the server
 
   module.exports = app;
