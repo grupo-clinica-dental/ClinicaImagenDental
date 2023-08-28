6b4ab916f421ee854fa422a3040fec5868b22bc1
CristianMartinez1398 <109698231+CristianMartinez1398@users.noreply.github.com>
2023-08-28 02:28:47 -0600

    terminado
    terminada la tabla de doctores

---
+++ database/funciones/fn_eliminar_doctores.sql
@@ -0,0 +1,43 @@
+CREATE OR REPLACE FUNCTION fn_crear_doctores(p_usuario_id int, p_fecha_borrado TIMESTAMP, p_color varchar) 
+RETURNS TABLE (
+    exito bool,
+    mensaje varchar(1000),
+    id_registro varchar(100)
+) AS $$
+
+DECLARE
+    v_exito bool := true;
+    v_mensaje varchar(1000);
+    v_id varchar(100);
+
+BEGIN
+    v_mensaje := 'Error en creación de usuario ' || p_usuario_id;
+
+    update tbl_doctores 
+    set estado = false
+    where id = $1
+
+    v_mensaje := 'Error en la inserción del log';
+
+    INSERT INTO tbl_log_de_acciones
+        (descripcion)
+    VALUES
+        ('Se crea el usuario ' || p_usuario_id);
+
+    v_mensaje := 'Operación Exitosa';
+
+    RETURN QUERY SELECT v_exito, v_mensaje, v_id;
+
+EXCEPTION
+    WHEN OTHERS THEN
+        INSERT INTO tbl_log_errores
+            (descripcion, proceso)
+        VALUES 
+            (v_mensaje || ' - ' || SQLERRM, 'fn_crear_doctores');
+
+        v_exito := false; 
+        v_mensaje := 'Operación Errónea - ' || SQLERRM;
+
+        RETURN QUERY SELECT v_exito, v_mensaje, p_usuario_id;
+END;
+$$ LANGUAGE plpgsql;
