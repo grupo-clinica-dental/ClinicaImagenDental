d96b87cbbd20ee2be7c8707976bb9937a3a7149a
CristianMartinez1398 <109698231+CristianMartinez1398@users.noreply.github.com>
2023-08-28 02:42:53 -0600

    f

--- database/funciones/fn_eliminar_doctores.sql
+++ database/funciones/fn_eliminar_doctores.sql
@@ -1,43 +1,38 @@
-CREATE OR REPLACE FUNCTION fn_crear_doctores(p_usuario_id int, p_fecha_borrado TIMESTAMP, p_color varchar) 
+CREATE OR REPLACE FUNCTION fn_eliminar_doctores(p_id int)
 RETURNS TABLE (
     exito bool,
     mensaje varchar(1000),
     id_registro varchar(100)
-) AS $$
+)
+AS $$
 
 DECLARE
     v_exito bool := true;
     v_mensaje varchar(1000);
-    v_id varchar(100);
 
 BEGIN
-    v_mensaje := 'Error en creación de usuario ' || p_usuario_id;
+    v_mensaje := 'Error en actualizar de usuario ' || p_id;
 
-    update tbl_doctores 
-    set estado = false
-    where id = $1
+    UPDATE tbl_doctores SET estado = 'false' WHERE id = p_id;
 
-    v_mensaje := 'Error en la inserción del log';
+    v_mensaje := 'Error en la insercion del log';
 
-    INSERT INTO tbl_log_de_acciones
-        (descripcion)
-    VALUES
-        ('Se crea el usuario ' || p_usuario_id);
+    INSERT INTO tbl_log_de_acciones (descripcion)
+    VALUES ('Se actualizó el usuario ' || p_id);
 
     v_mensaje := 'Operación Exitosa';
-
-    RETURN QUERY SELECT v_exito, v_mensaje, v_id;
+    RETURN QUERY SELECT v_exito, v_mensaje, p_id::varchar;
 
 EXCEPTION
     WHEN OTHERS THEN
-        INSERT INTO tbl_log_errores
-            (descripcion, proceso)
-        VALUES 
-            (v_mensaje || ' - ' || SQLERRM, 'fn_crear_doctores');
 
-        v_exito := false; 
-        v_mensaje := 'Operación Errónea - ' || SQLERRM;
+    INSERT INTO tbl_log_errores (descripcion, proceso)
+    VALUES (v_mensaje || ' - ' || SQLERRM, 'fn_eliminar_doctores');
 
-        RETURN QUERY SELECT v_exito, v_mensaje, p_usuario_id;
+    v_exito := false;
+    v_mensaje := 'Operación Erronea - ' || SQLERRM;
+
+    RETURN QUERY SELECT v_exito, v_mensaje, p_id::varchar;
+
 END;
 $$ LANGUAGE plpgsql;
