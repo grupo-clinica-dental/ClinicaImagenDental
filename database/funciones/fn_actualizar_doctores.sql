7bf49e66d7b600db0089f863a99d922dee37f301
CristianMartinez1398 <109698231+CristianMartinez1398@users.noreply.github.com>
2023-09-06 02:28:11 -0600

    Funciona
    Funciona  todo el get, post, put, delete

--- database/funciones/fn_actualizar_doctores.sql
+++ database/funciones/fn_actualizar_doctores.sql
@@ -1,34 +1,50 @@
-CREATE OR REPLACE FUNCTION fn_actualizar_doctores(p_nombre VARCHAR, p_correo_electronico VARCHAR, p_color VARCHAR, p_id INT) 
+CREATE OR REPLACE FUNCTION fn_actualizar_doctores(
+    p_nombre varchar,
+    p_correo_electronico varchar,
+    p_color varchar,
+    p_id int
+)
 RETURNS TABLE (
-    exito boolean, 
-    mensaje varchar(1000), 
+    exito bool,
+    mensaje varchar(1000),
     id_registro varchar(100)
 )
 AS $$
 DECLARE
-    v_exito boolean := true;
-    v_mensaje varchar(1000) := 'Operación Exitosa';
+    v_exito bool := true;
+    v_mensaje varchar(1000);
 BEGIN
-    -- Actualizar el registro del doctor
-    UPDATE tbl_doctores 
-    SET nombre = p_nombre, correo_electronico = p_correo_electronico, color = p_color 
+    v_mensaje := 'Error en actualizar el usuario ' || p_nombre;
+
+   
+    UPDATE tbl_doctores
+    SET nombre = p_nombre,
+        correo_electronico = p_correo_electronico,
+        color = p_color
     WHERE id = p_id;
 
-    -- Registrar la acción
+    v_mensaje := 'Error en la inserción del log';
+
+    
     INSERT INTO tbl_log_de_acciones (descripcion)
     VALUES ('Se actualizó el usuario ' || p_nombre);
 
-    RETURN QUERY SELECT v_exito, v_mensaje, p_id :: VARCHAR;
+    v_mensaje := 'Operación Exitosa';
 
-EXCEPTION 
-    WHEN OTHERS THEN
-        -- Registrar el error específico
-        INSERT INTO tbl_log_errores (descripcion, proceso)
-        VALUES (v_mensaje || ' - ' || SQLERRM, 'fn_actualizar_doctores');
-        
-        v_exito := false; 
-        v_mensaje := 'Operación Errónea - ' || SQLERRM;
+    
+    RETURN QUERY SELECT v_exito, v_mensaje, p_nombre;
 
-        RETURN QUERY SELECT v_exito, v_mensaje, p_id :: VARCHAR;
+EXCEPTION
+WHEN OTHERS THEN
+    
+    INSERT INTO tbl_log_errores (descripcion, proceso)
+    VALUES (v_mensaje || ' - ' || SQLERRM, 'fn_actualizar_doctores');
+
+    v_exito := false;
+    v_mensaje := 'Operación Errónea - ' || SQLERRM;
+
+    
+    RETURN QUERY SELECT v_exito, v_mensaje, p_nombre;
 END;
 $$ LANGUAGE plpgsql;
+
