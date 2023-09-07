72cdafafdb6157cab3b7342404aebb89808915d9
keven-bardales <122845766+keven-bardales@users.noreply.github.com>
2023-09-07 08:01:49 -0600

    feat(): actualizacion en el endpoint de usuarios con funciones sql

---
+++ database/funciones/fn_crear_usuario.sql
@@ -0,0 +1,44 @@
+-- Crear función para insertar un nuevo usuario en la tabla tbl_usuarios
+
+CREATE OR REPLACE FUNCTION FN_CREAR_USUARIO(P_NOMBRE 
+VARCHAR(100), P_EMAIL VARCHAR(100), P_TELEFONO VARCHAR
+(15), P_PASSWORD VARCHAR(255)) RETURNS TABLE(EXITO 
+BOOLEAN, MENSAJE VARCHAR(1000), ID_REGISTRO SERIAL
+) AS $$ 
+	$$ DECLARE v_exito BOOLEAN := TRUE;
+	v_mensaje VARCHAR(1000);
+	v_id SERIAL;
+	BEGIN
+	INSERT INTO
+	    tbl_usuarios (
+	        nombre,
+	        email,
+	        telefono,
+	        password
+	    )
+	VALUES (
+	        p_nombre,
+	        p_email,
+	        p_telefono,
+	        p_password
+	    ) RETURNING id INTO v_id;
+	INSERT INTO
+	    tbl_log_de_acciones (descripcion)
+	VALUES (
+	        'Usuario creado con ID ' || v_id
+	    );
+	v_mensaje := 'Usuario creado exitosamente con ID ' || v_id;
+	RETURN QUERY SELECT v_exito, v_mensaje, v_id;
+	EXCEPTION
+	WHEN OTHERS THEN
+	INSERT INTO
+	    tbl_log_errores (descripcion, proceso)
+	VALUES (
+	        'Error al crear el usuario: ' || SQLERRM,
+	        'fn_crear_usuario'
+	    );
+	v_exito := FALSE;
+	v_mensaje := 'Error al crear el usuario: ' || SQLERRM;
+	RETURN QUERY SELECT v_exito, v_mensaje, NULL;
+	END;
+	$$ LANGUAGE plpgsql;
